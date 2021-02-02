f829a52a1bef1afbeae13064468d1c8612c2c591
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-02-02 19:47:10 +0530

    styling: text centerd for sidebar and edu list

--- src/pages/MainScreen.tsx
+++ src/pages/MainScreen.tsx
@@ -1,13 +1,21 @@
-import { Container, makeStyles, Typography } from '@material-ui/core';
+import { Box, Container, makeStyles, Typography } from '@material-ui/core';
 import React, { FC } from 'react';
 import AddEducationModal from '../components/AddEducationModal';
 
 const useStyles = makeStyles({});
 const MainScreen: FC = () => {
   return (
-    <Container maxWidth='xs'>
+    <Container style={{ textAlign: 'center', paddingTop: '1rem' }}>
       <Typography>Welcome to Harsh's education page</Typography>
       <AddEducationModal />
+      <Box display='flex' mt={2}>
+        <Box flexGrow={1} style={{ backgroundColor: 'red' }}>
+          lorme
+        </Box>
+        <Box flexGrow={3} style={{ backgroundColor: 'green' }}>
+          lorme
+        </Box>
+      </Box>
     </Container>
   );
 };
